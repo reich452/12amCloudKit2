70359a23b56916073ab87445827d9919f27412a6
reich452 <reich452@regis.edu>
2017-11-03 18:44:07 -0600

    Added test login and welcome VC

---
+++ 12a.m./ViewControllers/TestLogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TestLogInViewController.swift
+//  12a.m.
+//
+//  Created by Nick Reichard on 11/3/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class TestLogInViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+}
---
+++ 12a.m./ViewControllers/WelcomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomViewController.swift
+//  12a.m.
+//
+//  Created by Nick Reichard on 11/3/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class WelcomViewController: UIViewController {
+    @IBOutlet weak var welcomLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
