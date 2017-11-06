8e5c279ef94aabfee45757b910666b2349b23f01
reich452 <reich452@regis.edu>
2017-11-06 15:35:33 -0700

    Added icons and Camera VC and Camera Preview VC

---
+++ 12a.m./ViewControllers/CameraPreviewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CameraPreviewViewController.swift
+//  12a.m.
+//
+//  Created by Nick Reichard on 11/6/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class CameraPreviewViewController: UIViewController {
+    @IBOutlet weak var captionTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ 12a.m./ViewControllers/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  12a.m.
+//
+//  Created by Nick Reichard on 11/6/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
