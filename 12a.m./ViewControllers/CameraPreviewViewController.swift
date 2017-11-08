8570051f40bdc012db121e6835ee3ed1d5310e31
reich452 <reich452@regis.edu>
2017-11-08 11:01:51 -0700

    Added takeCamera button, updated AVFoundation

--- 12a.m./ViewControllers/CameraPreviewViewController.swift
+++ 12a.m./ViewControllers/CameraPreviewViewController.swift
@@ -8,13 +8,164 @@
 
 import UIKit
 
-class CameraPreviewViewController: UIViewController {
+class CameraPreviewViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var captionTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
+    var location = CGPoint(x: 0, y: 0)
+    var capturedImage: UIImage?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imageView.image = capturedImage
+        
+        imageCaptionTextFieldGestures()
+        
+        captionTextField.delegate = self
+        
+        // Tap anywhere on the screen to dismiss the keyboard
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
     }
-
+    
+    // MARK: - TextField Functions
+    
+    func imageCaptionTextFieldGestures() {
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(CameraPreviewViewController.draggedView(_:)))
+        
+        captionTextField.addGestureRecognizer(panGesture)
+        captionTextField.isUserInteractionEnabled = true
+        
+    }
+    
+    // Press return to dismiss keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        captionTextField.resignFirstResponder()
+        return true
+    }
+    
+    // The following four functions allow the textField to be dragged around
+    
+    // Haven't tested but the userDragged function may not be needed since draggedView function does the same and sets bounds
+    func userDragged(gesture: UIPanGestureRecognizer){
+        let loc = gesture.location(in: self.view)
+        self.captionTextField.center = loc
+    }
+    
+    func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        let touch : UITouch! = touches.first as? UITouch
+        
+        location = touch.location(in: self.view)
+        
+        captionTextField.center = location
+    }
+    
+    func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        let touch : UITouch! = touches.first as? UITouch
+        
+        location = touch.location(in: self.view)
+        
+        captionTextField.center = location
+    }
+    
+    func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch : UITouch! = touches.first
+        
+        let location = touch.location(in: self.view)
+        
+        captionTextField.center = location
+    }
+    
+    // Function that sets bounds of dragging textField around screen
+    @objc func draggedView(_ sender: UIPanGestureRecognizer) {
+        
+        guard let senderView = sender.view else {
+            return
+        }
+        
+        var translation = sender.translation(in: view)
+        
+        translation.x = max(translation.x, imageView.frame.minX - captionTextField.frame.minX)
+        translation.x = min(translation.x, imageView.frame.maxX - captionTextField.frame.maxX)
+        
+        translation.y = max(translation.y, imageView.frame.minY - captionTextField.frame.minY)
+        translation.y = min(translation.y, imageView.frame.maxY - captionTextField.frame.maxY)
+        
+        senderView.center = CGPoint(x: senderView.center.x + translation.x, y: senderView.center.y + translation.y)
+        sender.setTranslation(.zero, in: view)
+        view.bringSubview(toFront: senderView)
+    }
+    
+    
+    // MARK: - Image Gesture Functions
+    
+    // Function to add tap gesture to UIImageView
+    //    func imageTapGesture() {
+    //
+    //        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+    //
+    //        imageView.addGestureRecognizer(tap)
+    //
+    //        imageView.isUserInteractionEnabled = true
+    //
+    //        self.view.addSubview(view)
+    //
+    //    }
+    
+    // Function which brings up textField when UIImageView is tapped, called in imageTapGesture
+    
+    // MARK: - Image Gesture Functions
+    
+    // Function to add tap gesture to UIImageView
+    //    func imageTapGesture() {
+    //
+    //        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+    //
+    //        imageView.addGestureRecognizer(tap)
+    //
+    //        imageView.isUserInteractionEnabled = true
+    //
+    //        self.view.addSubview(view)
+    //
+    //    }
+    
+    //     Function which brings up textField when UIImageView is tapped, called in imageTapGesture
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        _ = tapGestureRecognizer.view as! UIImageView
+        
+        // Your action
+    }
+    
+    
+    // MARK: - Action
+    
+    @IBAction func usePhotoButtonTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            
+            self.usePhoto()
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func imageToTextFieldButton(_ sender: Any) {
+        
+        
+    }
+    
+    func usePhoto() {
+        guard let commentText = captionTextField.text, let image = imageView.image else { return }
+        
+        PostController.shared.createPost(image: image, text: commentText) { (post) in
+            
+        }
+        // TODO: post
+    }
 }
+
+
--- 12a.m./ViewControllers/CameraViewController.swift
+++ 12a.m./ViewControllers/CameraViewController.swift
@@ -7,12 +7,165 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
-
+    
+    // MARK: - TODO:  Call the CamearController (Same functions, but doesn't work)
+    
+    // MARK: - Properties
+    
+    fileprivate let captureSession = AVCaptureSession()
+    fileprivate var camera: AVCaptureDevice!
+    fileprivate var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    fileprivate var cameraCaptureOutput: AVCapturePhotoOutput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initializeCaptureSession()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.barTintColor = UIColor.clear
     }
-
+    
+    // MARK: - Actions
+    
+    @IBAction func cameraToggleButton(_ sender: Any) {
+        switchCameraInput()
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        
+        takePicture()
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Main
+    
+    func initializeCaptureSession() {
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        camera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let cameraCaptureInput = try AVCaptureDeviceInput(device: camera)
+            cameraCaptureOutput = AVCapturePhotoOutput()
+            
+            captureSession.addInput(cameraCaptureInput)
+            captureSession.addOutput(cameraCaptureOutput!)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.bounds
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+        captureSession.startRunning()
+    }
+    
+    func takePicture() {
+        // Many settings to customize
+        let settings = AVCapturePhotoSettings()
+        settings.flashMode = .auto
+        
+        cameraCaptureOutput?.capturePhoto(with: settings, delegate: self)
+    }
+    
+    // MARK: - Toggle
+    
+    func switchCameraInput() {
+        self.captureSession.beginConfiguration()
+        
+        var existingConnection: AVCaptureDeviceInput?
+        var newInput: AVCaptureDeviceInput?
+        var newCamera: AVCaptureDevice?
+        
+        for connection in self.captureSession.inputs {
+            guard let input = connection as? AVCaptureDeviceInput else { return }
+            if input.device.hasMediaType(AVMediaType.video) {
+                existingConnection = input
+            }
+        }
+        self.captureSession.removeInput(existingConnection!)
+        
+        if let oldCamera = existingConnection {
+            if oldCamera.device.position == .back {
+                newCamera = self.cameraWithPosition(position: .front)
+            } else {
+                newCamera = self.cameraWithPosition(position: .back)
+            }
+        }
+        
+        do {                                        // TODO: Safley unwrap newCamera
+            newInput = try AVCaptureDeviceInput(device: newCamera!)
+            self.captureSession.addInput(newInput!)
+        } catch {
+            print("Error: Cannot Capure newInput \(error.localizedDescription)")
+        }
+        
+        self.captureSession.commitConfiguration()
+    }
 }
+
+extension CameraViewController : AVCapturePhotoCaptureDelegate {
+    
+    // MARK: Display Photo
+    
+    func displayCapturedPhoto(capturedPhoto : UIImage) {
+        
+        let imagePreviewViewController = storyboard?.instantiateViewController(withIdentifier: "ImagePreviewViewController") as! CameraPreviewViewController
+        imagePreviewViewController.capturedImage = capturedPhoto
+        navigationController?.pushViewController(imagePreviewViewController, animated: true)
+    }
+    
+//    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+//
+//        if let error = error {
+//            print(error.localizedDescription)
+//        } else {
+//            if let sampleBuffer = photoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) {
+//
+//
+//                if let finalImage = UIImage(data: dataImage) {
+//
+//                    displayCapturedPhoto(capturedPhoto: finalImage)
+//                }
+//            }
+//        }
+//    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            print("Did not finish processing photot \(error.localizedDescription)")
+        }
+        guard let dataImage = photo.fileDataRepresentation(),
+            let finalImage = UIImage(data: dataImage) else { return }
+        displayCapturedPhoto(capturedPhoto: finalImage)
+    }
+}
+
+// MARK: - Handle Positions
+
+extension CameraViewController {
+    
+    func cameraWithPosition(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        // A query for finding and monitoring available capture devices
+        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified) as AVCaptureDevice.DiscoverySession
+        
+        for device in discovery.devices as [AVCaptureDevice] {
+            if device.position == position {
+                return device
+            }
+        }
+        return nil
+    }
+}
+
+
