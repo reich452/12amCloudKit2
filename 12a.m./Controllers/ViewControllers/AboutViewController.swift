0c75cef8799f30d896133ba0311a6d9eca866e55
reich452 <reich452@regis.edu>
2017-11-27 12:35:15 -0700

    Added Terms and Conditions/ Privacy Polocy

--- 12a.m./Controllers/ViewControllers/AboutViewController.swift
+++ 12a.m./Controllers/ViewControllers/AboutViewController.swift
@@ -11,14 +11,25 @@ import UIKit
 class AboutViewController: UIViewController {
     
     @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpTermsAndConditions()
     }
     // MARK: - Actions
     @IBAction func closeButtonTapped(_ sender: UIButton) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
+    func setUpTermsAndConditions() {
+        if let htmlFile = Bundle.main.path(forResource: "Terms", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData as Data, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
 }
+
+
