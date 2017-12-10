05e4bbccf13ed2a20e3939c0d463d34a97250316
reich452 <reich452@regis.edu>
2017-12-10 15:34:10 -0700

    Updated profileDetail VC to contain CLLocationDelegate

--- 12a.m./Controllers/ViewControllers/ProfileDetailTableViewController.swift
+++ 12a.m./Controllers/ViewControllers/ProfileDetailTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ProfileDetailTableViewController: UITableViewController {
+class ProfileDetailTableViewController: UITableViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var countryLabel: UILabel!
@@ -16,25 +17,90 @@ class ProfileDetailTableViewController: UITableViewController {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpView()
         self.setUpAppearance()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.cityLabel.text = "\(TimeZone.current)"
     }
     
-    var comment: Comment?
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.locationManager.stopUpdatingLocation()
+    }
     
-   private func setUpView() {
+    // MARK: - Properties
+    
+    var comment: Comment?
+    private let locationManager = CLLocationManager()
+    
+    // MARK: - View
+    
+    private func setUpView() {
         guard let owner = comment?.owner else { return }
         self.usernameLabel.text = owner.username
         self.profileImageView.image = owner.photo
     }
     
-   private func setUpAppearance() {
+    private func setUpAppearance() {
         self.profileImageView.layer.cornerRadius = self.profileImageView.layer.frame.height / 2 
         self.profileImageView.clipsToBounds = true
         self.profileImageView.contentMode = .scaleAspectFill
-        
     }
+}
 
+// MARK: - CLLocationManagerDelegate 
+
+extension ProfileDetailTableViewController {
+    
+    // MARK: - Location
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = manager.location else { return }
+        CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error) -> Void in
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                return
+            }
+            if (placemarks?.count)! > 0 {
+                
+                print("placemarks",placemarks ?? 0)
+                let pm = placemarks?[0]
+                self.displayLocationInfo(pm)
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    func displayLocationInfo(_ placemark: CLPlacemark?) {
+        if let containsPlacemark = placemark {
+            
+            print("your location is:-",containsPlacemark)
+            //stop updating location to save battery life
+            locationManager.stopUpdatingLocation()
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+            let state = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+            
+            let unitedStates = "United States"
+            if (country?.contains(unitedStates))! {
+                self.countryLabel.text = "U.S.A"
+            } else {
+                self.countryLabel.text = country
+            }
+            self.cityLabel.text = locality
+            self.cityLabel.text = state
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
 }
