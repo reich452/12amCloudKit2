eabaea040a616c68d01e487e1e76b87c90c62fc7
reich452 <reich452@regis.edu>
2018-01-01 14:18:19 -0700

    Revered to commit f7b5ef151bff29aacd06b2f5ae5efcae3345e9e2

--- 12a.m./Controllers/CustomViewController.swift
+++ 12a.m./Controllers/CustomViewController.swift
@@ -13,34 +13,7 @@ class CustomViewController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
-        self.customTabBar()
-    }
-    
-    func customTabBar() {
-        let storyboard1 = UIStoryboard(name: "Main", bundle: nil)
-        let navigationController = storyboard1.instantiateViewController(withIdentifier: "feedTVC")
-        navigationController.title = "Home"
-        navigationController.tabBarItem.image = #imageLiteral(resourceName: "homeIcon")
-        
-        let storyboard2 = UIStoryboard(name: "Main", bundle: nil)
-        let updateProfileVC = storyboard2.instantiateViewController(withIdentifier: "updateProfileVC")
-        updateProfileVC.title = "Profile"
-       updateProfileVC.tabBarItem.image = #imageLiteral(resourceName: "profileIcon")
-        
-//        let storyboard3 = UIStoryboard(name: "Main", bundle: nil)
-//        let saveSearchTVC = storyboard3.instantiateViewController(withIdentifier: "savedImages")
-//        let thridNavigationController = UINavigationController(rootViewController: saveSearchTVC)
-//        thridNavigationController.title = "Saved"
-//        thridNavigationController.tabBarItem.image = #imageLiteral(resourceName: "heartIcon")
-        
-        viewControllers = [navigationController, updateProfileVC]
-        tabBar.isTranslucent = true
-        
-        let topBorder = CALayer()
-        topBorder.frame = CGRect(x: 0, y: 0, width: 1000, height: 0.5)
-        topBorder.backgroundColor = UIColor.rgb(229, green: 231, blue: 235).cgColor
-        tabBar.layer.addSublayer(topBorder)
-        tabBar.clipsToBounds = true
+       
     }
     
 }
