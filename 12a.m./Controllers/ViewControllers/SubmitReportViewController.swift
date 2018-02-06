2e515284cfc4e9f9e626f7ea4c4165ff66ab29af
reich452 <reich452@regis.edu>
2018-02-06 09:05:57 -0700

    Updated report model

---
+++ 12a.m./Controllers/ViewControllers/SubmitReportViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SubmitReportViewController.swift
+//  12a.m.
+//
+//  Created by Nick Reichard on 2/5/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class SubmitReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 12a.m./Model/Report.swift
+++ 12a.m./Model/Report.swift
@@ -15,8 +15,9 @@ class Report {
     static let recordTypeKey = "Report"
     fileprivate let titleKey = "title"
     fileprivate let descriptionKey = "description"
-    fileprivate let ownerReferenceKey = "ownerRefKey"
+    fileprivate let ownerReferenceKey = "ownerReference"
     fileprivate let reportReferenceKey = "reportReference"
+    fileprivate let postReferenceKey = "postReference"
     
     // MARK: - Properties
     
@@ -25,24 +26,28 @@ class Report {
     var description: String?
     var owner: User?
     var ckRecordID: CKRecordID?
+    var postReference: CKReference
   
     
-    init(title: String, description: String? = String(), ownerReference: CKReference, owner: User) {
+    init(title: String, description: String? = String(), ownerReference: CKReference, owner: User?, postReference: CKReference) {
         self.title = title
         self.description = description
         self.ownerReference = ownerReference
         self.owner = owner
+        self.postReference = postReference
        
     }
     
     init?(ckRecord: CKRecord) {
         guard let title = ckRecord[titleKey] as? String,
             let ownerReference = ckRecord[ownerReferenceKey] as? CKReference,
-            let description = ckRecord[descriptionKey] as? String else { return nil }
+            let description = ckRecord[descriptionKey] as? String,
+            let postReference = ckRecord[postReferenceKey] as? CKReference else { return nil }
         
         self.title = title
         self.ownerReference = ownerReference
         self.description = description
+        self.postReference = postReference
         self.ckRecordID = ckRecord.recordID
 
     }
@@ -52,12 +57,17 @@ extension CKRecord {
     
     convenience init(report: Report) {
          let recordID = report.ckRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        guard let user = report.owner else {
+            fatalError("report doesn't have a owner")
+        }
         
         self.init(recordType: Report.recordTypeKey, recordID: recordID)
         self.setValue(report.title, forKeyPath: report.titleKey)
-        self.setValue(report.ownerReference, forKeyPath: report.ownerReferenceKey)
         self.setValue(report.description, forKeyPath: report.descriptionKey)
-     
+        self.setValue(user.appleUserRef, forKey: report.ownerReferenceKey)
+        self.setValue(report.ownerReference, forKey: report.reportReferenceKey)
+        self.setValue(report.postReference, forKey: report.postReferenceKey)
+    
     }
 }
 
